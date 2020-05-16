9bf7aee1e37dea1398ab2d6796d8c123973ea4b5
Ben PicklesOnion <39406726+RabidMexican@users.noreply.github.com>
2020-05-16 14:08:27 +0200

    readded the deafult page under header (broke the default css but I don't mind)

--- src/App.js
+++ src/App.js
@@ -9,11 +9,24 @@ function App() {
     
     <div className="App">
       <HeaderComponent/>
-      <div>
-        Learn React
-      </div>
+      <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
     </div>
-    
+
   );
 }
 
