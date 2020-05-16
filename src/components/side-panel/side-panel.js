60d421bf80d4c4b5b46ef38235a986fc32c5cf36
Patrick Mettraux <patrick@mettraux-web.net>
2020-05-16 16:37:33 +0200

    first version is done

--- src/components/side-panel/side-panel.js
+++ src/components/side-panel/side-panel.js
@@ -1,8 +1,6 @@
 import React from "react";
-import IconButton from '@material-ui/core/IconButton'
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
 import NavigateNext from '@material-ui/icons/NavigateNext';
-import NavigateBefore from '@material-ui/icons/NavigateBefore';
 import "./side-panel.scss";
 
 export default class SidePanelComponent extends React.Component {
@@ -20,17 +18,11 @@ export default class SidePanelComponent extends React.Component {
   }
 
   render() {
-    let iconButton;
-    if (this.state.isOpen) {
-      iconButton = <NavigateBefore color="grey" />;
-    } else {
-      iconButton = <NavigateNext color="grey" />;
-    }
     return (
       <div className="side-panel">  
-        <IconButton onClick={this.toggleDrawer} className="button-toggle">
-            {iconButton}
-        </IconButton>      
+        <div className={'button-toggle' + (this.state.isOpen ? ' open' : '') } onClick={this.toggleDrawer}>
+        <NavigateNext color="grey" />
+        </div>      
         <SwipeableDrawer
           anchor="left"
           open={this.state.isOpen}
