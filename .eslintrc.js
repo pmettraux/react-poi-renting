12e3d05d35d481a2ddf4f6d05f335534e5f85968
Patrick Mettraux <patrick@mettraux-web.net>
2020-05-16 10:13:18 +0200

    Circleci project setup (#6)
    * Add .circleci/config.yml
    
    * base circleci config
    
    * fixed linter
    
    * fixed tests
    
    close #3

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    'plugin:react/recommended',
+    // 'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+  overrides: [
+    {
+      files: [
+        "**/*.spec.js",
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+};
