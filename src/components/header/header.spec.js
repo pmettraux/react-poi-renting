7500794dd577044675b657d973180b37005194b8
Ben PicklesOnion <39406726+RabidMexican@users.noreply.github.com>
2020-05-16 13:57:58 +0200

    Fixed more bad references & readded tests (thanks lint)

--- src/components/header/header.spec.js
+++ src/components/header/header.spec.js
@@ -3,5 +3,7 @@ import { render } from '@testing-library/react';
 import HeaderComponent from './header';
 
 test('renders header component', () => {
-  const { getByText } = render(<HeaderCompontent/>);
+  const { getByText } = render(<HeaderComponent/>);
+  const linkElement = getByText(/PLEASE INSERT APP TITLE HERE LEL/i);
+  expect(linkElement).toBeInTheDocument();
 });
