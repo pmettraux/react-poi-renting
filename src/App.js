6d1ceef63bbdb6cc9e758372a2c5c2609b9cc6f9
Ben PicklesOnion <39406726+RabidMexican@users.noreply.github.com>
2020-05-16 14:03:42 +0200

    restructured div nesting to comply to React

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 import HeaderComponent from './components/header/header';
 
 function App() {
   return (
+    
     <div className="App">
       <HeaderComponent/>
+      <div>
+        Learn React
+      </div>
     </div>
-    <div>
-      Learn React
-    </div>
+    
   );
 }
 
