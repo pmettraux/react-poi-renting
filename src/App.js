9d5b3a624eb65d62394477412032e48fe8bde217
Ben PicklesOnion <39406726+RabidMexican@users.noreply.github.com>
2020-05-16 12:58:30 +0200

    finished initial creation of header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-import HeaderComponent from './components/header-component/header-component.js';
 
 function App() {
   return (
     <div className="App">
-        <HeaderComponent></HeaderComponent>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
