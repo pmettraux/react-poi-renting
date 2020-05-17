eb962b05c4067d720062a17a0646a4b36889af01
Patrick Mettraux <patrick@mettraux-web.net>
2020-05-17 10:10:48 +0200

    Feat/side panel (#34)
    * basic bahavior of the side panel
    
    * attempt with drawer
    
    * first version is done

---
+++ src/components/side-panel/side-panel.js
@@ -0,0 +1,36 @@
+import React from "react";
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
+import NavigateNext from '@material-ui/icons/NavigateNext';
+import "./side-panel.scss";
+
+export default class SidePanelComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: false };
+
+    this.toggleDrawer = this.toggleDrawer.bind(this);
+  }
+
+  toggleDrawer() {
+    this.setState(state => ({
+      isOpen: !state.isOpen
+    }));
+  }
+
+  render() {
+    return (
+      <div className="side-panel">  
+        <div className={'button-toggle' + (this.state.isOpen ? ' open' : '') } onClick={this.toggleDrawer}>
+        <NavigateNext color="grey" />
+        </div>      
+        <SwipeableDrawer
+          anchor="left"
+          open={this.state.isOpen}
+          onClose={this.toggleDrawer}
+          onOpen={this.toggleDrawer}
+        >asdasdas
+        </SwipeableDrawer>
+      </div>
+    );
+  }
+}
