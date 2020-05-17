24cfb04db52a5c8586fa73e01d42eb13e94c880d
Ben PicklesOnion <39406726+RabidMexican@users.noreply.github.com>
2020-05-17 10:40:56 +0200

    removed menu button because side-panel makes it redundant

--- src/components/main-header/main-header.js
+++ src/components/main-header/main-header.js
@@ -3,8 +3,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import "./main-header.scss";
 
 export default class MainHeader extends React.Component {
@@ -14,9 +12,6 @@ export default class MainHeader extends React.Component {
             <div className="main-header">
                 <AppBar position="static">
                     <Toolbar>
-                        <IconButton edge="start" className="icon-button" color="inherit" aria-label="menu">
-                            <MenuIcon />
-                        </IconButton>
                         <Typography variant="h6" className="title">
                             APP TITLE
                         </Typography>
