d8c46bb7bbef4fecfafc728ad9c7e7442473451e
Kysim <yosh97@hotmail.com>
2020-05-18 22:09:29 +0200

    Correction of css

--- src/components/map-component/map.js
+++ src/components/map-component/map.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
-import './map.scss'
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import * as locData from "./data/location-data.json";
+import L from 'leaflet';
+
+//Correction of the invisble icon
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+const lat = 46.292894;
+const lng = 7.536433;
+const zoom = 10;
 
 export default class LeafletMapComponent extends Component {
     constructor() {
         super();
         this.state = {
-            lat: 46.292894,
-            lng: 7.536433,
-            zoom: 10,
+            lat: lat,
+            lng: lng,
+            zoom: zoom,
         };
     }
 
@@ -17,19 +29,21 @@ export default class LeafletMapComponent extends Component {
         const position = [this.state.lat, this.state.lng]
         return (
             <div>
-                <Map center={position} zoom={this.state.zoom} style={{ width: '100%', height: '980px' }}>
+                <Map center={position} zoom={this.state.zoom}>
                     <TileLayer
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
                     {locData.features.map((loc) => (
-                        <Marker onClick={this.onMarkerClick}
+                        <Marker
                             key={loc.properties.LOC_ID}
                             position={{
                                 lat: loc.geometry.coordinates[0],
                                 lng: loc.geometry.coordinates[1]
                             }}
-                            name={loc.properties.NAME} />
+                            name={loc.properties.NAME}
+                            description={loc.properties.DESCRIPTION} 
+                        />
                     ))
                     }
                     <Marker position={position}>
