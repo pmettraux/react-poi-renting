4885eef23f844793bba43bcf5e14ab71d6216e80
Ben PicklesOnion <39406726+RabidMexican@users.noreply.github.com>
2020-05-20 13:07:03 +0200

    Fix/map height (#41)
    * fixed map height using flexboxes, separated map css, general tidy
    
    * fixed map height using flexboxes, separated map css, general tidy
    
    * fixed map height using flexboxes, separated map css, general tidy
    
    * readded color:gray to toggle text in sidebar scss

--- src/components/main-header/main-header.js
+++ src/components/main-header/main-header.js
@@ -1,10 +1,8 @@
 import React from "react";
-
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
-
 import SearchIcon from '@material-ui/icons/Search';
 import "./main-header.scss";
 
