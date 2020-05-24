d1a6bde70ed3a87b7e45136fe6e2da33a010684f
Kysim <yosh97@hotmail.com>
2020-05-24 20:04:31 +0200

    Adding branch and some tries

--- src/components/map-component/map.js
+++ src/components/map-component/map.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import * as locData from "./data/location-data.json";
 import L from 'leaflet';
+import { geolocated } from 'react-geolocated';
 
 //Correction of the invisble icon
 delete L.Icon.Default.prototype._getIconUrl;
@@ -13,27 +14,36 @@ L.Icon.Default.mergeOptions({
 
 const DEFAULT_LATITUDE = 46.292894;
 const DEFAULT_LONGITUDE = 7.536433;
-const DEFAULT_ZOOM= 10;
+const DEFAULT_ZOOM = 10;
 
-export default class LeafletMapComponent extends Component {
+class LeafletMapComponent extends Component {
     constructor() {
         super();
         this.state = {
-            lat: DEFAULT_LATITUDE,
-            lng: DEFAULT_LONGITUDE,
             zoom: DEFAULT_ZOOM,
         };
     }
 
     render() {
-        const position = [this.state.lat, this.state.lng]
+        const longitude = this.props.coords ? this.props.coords.longitude : DEFAULT_LONGITUDE;
+        const latitude = this.props.coords ? this.props.coords.latitude : DEFAULT_LATITUDE;
         return (
             <div>
-                <Map center={position} zoom={this.state.zoom}>
+                <Map center={[ longitude, latitude ]} zoom={this.state.zoom}>
                     <TileLayer
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
+                    {
+                        !this.props.coords ?
+                            <div className="loading">Loading</div>
+                            :
+                            <Marker position={[longitude, latitude]}>
+                                <Popup>
+                                    You're here!
+                                </Popup>
+                            </Marker>
+                    }
                     {locData.features.map((loc) => (
                         <Marker
                             key={loc.properties.LOC_ID}
@@ -42,17 +52,18 @@ export default class LeafletMapComponent extends Component {
                                 lng: loc.geometry.coordinates[1]
                             }}
                             name={loc.properties.NAME}
-                            description={loc.properties.DESCRIPTION} 
+                            description={loc.properties.DESCRIPTION}
                         />
                     ))
                     }
-                    <Marker position={position}>
-                        <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
-                        </Popup>
-                    </Marker>
                 </Map>
             </div>
         )
     }
 }
+export default geolocated({
+    positionOptions: {
+        enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 10000
+})(LeafletMapComponent)
