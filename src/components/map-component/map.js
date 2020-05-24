f03df8838743a296c32901725e5e54dda5e5906a
Kysim <yosh97@hotmail.com>
2020-05-24 20:28:07 +0200

    Adding geoloc

--- src/components/map-component/map.js
+++ src/components/map-component/map.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import * as locData from "./data/location-data.json";
 import L from 'leaflet';
-import { geolocated } from 'react-geolocated';
 
 //Correction of the invisble icon
 delete L.Icon.Default.prototype._getIconUrl;
@@ -16,34 +15,44 @@ const DEFAULT_LATITUDE = 46.292894;
 const DEFAULT_LONGITUDE = 7.536433;
 const DEFAULT_ZOOM = 10;
 
-class LeafletMapComponent extends Component {
+export default class LeafletMapComponent extends Component {
     constructor() {
         super();
         this.state = {
+            lat: DEFAULT_LATITUDE,
+            lng: DEFAULT_LONGITUDE,
             zoom: DEFAULT_ZOOM,
         };
     }
 
+    getCurrentLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    this.setState({
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    });
+                }
+            )
+        } else {
+            console.log("Cannot get location");
+        }
+    }
+
+    componentDidMount(){
+        this.getCurrentLocation(); //get the location trough the web browser
+    }
+
     render() {
-        const longitude = this.props.coords ? this.props.coords.longitude : DEFAULT_LONGITUDE;
-        const latitude = this.props.coords ? this.props.coords.latitude : DEFAULT_LATITUDE;
+        const position = [this.state.lat, this.state.lng]
         return (
             <div>
-                <Map center={[ longitude, latitude ]} zoom={this.state.zoom}>
+                <Map center={position} zoom={this.state.zoom}>
                     <TileLayer
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    {
-                        !this.props.coords ?
-                            <div className="loading">Loading</div>
-                            :
-                            <Marker position={[longitude, latitude]}>
-                                <Popup>
-                                    You're here!
-                                </Popup>
-                            </Marker>
-                    }
                     {locData.features.map((loc) => (
                         <Marker
                             key={loc.properties.LOC_ID}
@@ -56,14 +65,13 @@ class LeafletMapComponent extends Component {
                         />
                     ))
                     }
+                    <Marker position={position}>
+                        <Popup>
+                            You're here.
+                        </Popup>
+                    </Marker>
                 </Map>
             </div>
         )
     }
-}
-export default geolocated({
-    positionOptions: {
-        enableHighAccuracy: false,
-    },
-    userDecisionTimeout: 10000
-})(LeafletMapComponent)
+} 
