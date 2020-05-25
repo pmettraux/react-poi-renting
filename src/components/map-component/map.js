b280267d1379fcf7c696955c24c8cd24de66baf6
Kysim <37473078+Kysim@users.noreply.github.com>
2020-05-25 11:18:49 +0200

    Geolocation (#44)
    * installation react-google-map
    
    * Add map to project
    
    * Add map
    
    * Adding map
    
    * Change map to leaflet
    
    * Correction package.json
    
    * Correction of css
    
    * constants
    
    * constants & constructor
    
    * Adding branch and some tries
    
    * Adding geoloc
    
    * Some corrections
    
    * Some corrections
    
    * Bug fixed

--- src/components/map-component/map.js
+++ src/components/map-component/map.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import * as locData from "./data/location-data.json";
 import L from 'leaflet';
-import './map.scss';
 
 //Correction of the invisble icon
 delete L.Icon.Default.prototype._getIconUrl;
@@ -14,23 +13,42 @@ L.Icon.Default.mergeOptions({
 
 const DEFAULT_LATITUDE = 46.292894;
 const DEFAULT_LONGITUDE = 7.536433;
-const DEFAULT_ZOOM= 10;
+const DEFAULT_ZOOM = 10;
 
 export default class LeafletMapComponent extends Component {
     constructor() {
         super();
-        this.state = {
+        this.state={
             lat: DEFAULT_LATITUDE,
             lng: DEFAULT_LONGITUDE,
             zoom: DEFAULT_ZOOM,
         };
     }
 
+    getCurrentLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    this.setState({
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    });
+                }
+            )
+        } else {
+            console.log("Cannot get location");
+        }
+    }
+
+    componentDidMount(){
+        this.getCurrentLocation(); //get the location trough the web browser
+    }
+
     render() {
-        const position = [this.state.lat, this.state.lng]
+        const position=[this.state.lat, this.state.lng]
         return (
-            <div className='map-wrapper'>
-                <Map center={position} zoom={this.state.zoom}>
+            <div>
+                <Map center = {position} zoom = {this.state.zoom}>
                     <TileLayer
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -43,17 +61,17 @@ export default class LeafletMapComponent extends Component {
                                 lng: loc.geometry.coordinates[1]
                             }}
                             name={loc.properties.NAME}
-                            description={loc.properties.DESCRIPTION} 
+                            description={loc.properties.DESCRIPTION}
                         />
                     ))
                     }
-                    <Marker position={position}>
+                    <Marker position = {position}>
                         <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
+                            You&apos;re here.
                         </Popup>
                     </Marker>
                 </Map>
             </div>
         )
     }
-}
+} 
