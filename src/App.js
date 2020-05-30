8ffb9bac3b9e986098af5f8677f96836db67864b
Patrick Mettraux <patrick@mettraux-web.net>
2020-05-30 11:01:35 +0200

    added pretty much everything that was missing...

--- src/App.js
+++ src/App.js
@@ -4,8 +4,21 @@ import './App.css';
 import MainHeader from './components/main-header/main-header';
 import SidePanelComponent from './components/side-panel/side-panel';
 import LeafletMapComponent from './components/map-component/map';
+import Loading from './components/loading/loading';
+import { useAuth0 } from './shared/react-auth0-spa';
 
 function App() {
+
+  // console.log('useAuth0', useAuth0());
+
+  let {
+    loading,
+  } = useAuth0();
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <div className="App">
       <MainHeader />
