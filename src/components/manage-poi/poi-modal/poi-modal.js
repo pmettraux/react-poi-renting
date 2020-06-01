b3f8c4b59ea8ce9cc308f7931815514cfc3abb3e
Patrick Mettraux <patrick@mettraux-web.net>
2020-06-01 18:27:28 +0200

    form to create POI

--- src/components/manage-poi/poi-modal/poi-modal.js
+++ src/components/manage-poi/poi-modal/poi-modal.js
@@ -10,15 +10,34 @@ import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import '../poi-modal/poi-modal.scss'
+import Grid from '@material-ui/core/Grid';
+import FormControl from '@material-ui/core/FormControl';
+import IconButton from '@material-ui/core/IconButton';
+import MyLocationIcon from '@material-ui/icons/MyLocation';
 
 export default class FormDialog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        open: false
+        open: false,
+        geolocationAvailable: false,
+        form: {
+            lat: null,
+            lng: null,
+            name: null,
+            description: null,
+        }
     };
     this.handleClickOpen = this.handleClickOpen.bind(this);
     this.handleClose = this.handleClose.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.getMyLocation = this.getMyLocation.bind(this);
+  }
+
+  componentDidMount() {
+    if ('geolocation' in navigator) {
+        this.setState({ geolocationAvailable: true });
+    }
   }
 
   handleClickOpen() {
@@ -33,6 +52,27 @@ export default class FormDialog extends React.Component {
     }));
   }
 
+  handleChange(event) {
+    this.setState({
+      form: {[event.target.name]: event.target.value},
+    });
+  }
+
+  createPoi() {
+    console.log('TODO: CREATE POI');
+  }
+
+  getMyLocation() {
+    navigator.geolocation.getCurrentPosition((position) => {
+        this.setState({
+            form: {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+            }
+        });
+    });
+  }
+
   render() {
     return (
         <div>
@@ -42,36 +82,82 @@ export default class FormDialog extends React.Component {
                 </Fab>
             </div>
             <Dialog open={this.state.open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title">New POI</DialogTitle>
-                <DialogContent>
-                <DialogContentText>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </DialogContentText>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="name"
-                    label="Name"
-                    type="text"
-                    fullWidth
-                />
-                <div className="upload-poi-picture">
-                    <Button component="label">
-                        <CloudUploadIcon color="primary"></CloudUploadIcon>
-                        <input type="file" style={{ display: "none" }}/>
+                <form onSubmit={this.createPoi}>
+                    <DialogTitle id="form-dialog-title">New POI</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Please fill the information in the form bellow
+                        </DialogContentText>
+                        <Grid container spacing={3}>
+                            <Grid item xs={4}>
+                                <TextField 
+                                    id="lat" 
+                                    label="Latitude" 
+                                    margin="dense"
+                                    variant="outlined"
+                                    value={this.state.form.lat}
+                                    onChange={this.handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={4}>
+                                <TextField 
+                                    id="lng" 
+                                    label="Longitude" 
+                                    margin="dense"
+                                    variant="outlined"
+                                    value={this.state.form.lng}
+                                    onChange={this.handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={4}>
+                                {
+                                    this.state.geolocationAvailable && (
+                                    <IconButton onClick={this.getMyLocation}>
+                                        <MyLocationIcon />
+                                    </IconButton>
+                                    )
+                                }
+                            </Grid>
+                        </Grid>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            label="Name"
+                            type="text"
+                            fullWidth
+                            variant="outlined"
+                            value={this.state.form.name}
+                            onChange={this.handleChange}
+                        />
+                        <TextField
+                            id="description"
+                            label="Description"
+                            margin="dense"
+                            multiline
+                            rows={4}
+                            fullWidth
+                            variant="outlined"
+                            value={this.state.form.description}
+                            onChange={this.handleChange}
+                        />
+                        <div className="upload-poi-picture">
+                            <Button component="label">
+                                <CloudUploadIcon color="primary"></CloudUploadIcon>
+                                <input type="file" style={{ display: "none" }}/>
+                            </Button>
+                            <span>Upload a picture of the POI</span>
+                        </div>
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={this.handleClose} color="primary">
+                        Cancel
                     </Button>
-                    <span>Upload a picture of the POI</span>
-                </div>
-                </DialogContent>
-                <DialogActions>
-                <Button onClick={this.handleClose} color="primary">
-                    Cancel
-                </Button>
-                <Button onClick={this.handleClose} color="primary">
-                    Submit
-                </Button>
-                </DialogActions>
+                    <Button onClick={this.handleClose} color="primary">
+                        Submit
+                    </Button>
+                    </DialogActions>
+                </form>
             </Dialog>
         </div>
     );
