ce7c7dd200dede1847b69d60f1ac3d474c724b68
Patrick Mettraux <patrick@mettraux-web.net>
2020-06-06 14:11:56 +0200

    improve structure (#50)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
 import FormDialog from './components/manage-poi/poi-modal/poi-modal';
 import MainHeader from './components/main-header/main-header';
 import SidePanelComponent from './components/side-panel/side-panel';
 import LeafletMapComponent from './components/map-component/map';
 import Loading from './components/loading/loading';
 import { useAuth0 } from './shared/react-auth0-spa';
+import { getPois } from './shared/api.service';
 
 
 function App() {
-  let {
+  const [pois, setPois] = useState([]);
+  const {
     loading,
     logout,
+    getTokenSilently,
     loginWithRedirect,
     isAuthenticated,
   } = useAuth0();
+
+  const getListPois = async() => {
+    const results = await getPois(getTokenSilently, loginWithRedirect);
+    const pois = results.data.map(poi => {
+      return {
+        key: poi.id,
+        position: {
+          lat: poi.lat,
+          lng: poi.lng,
+        },
+        name: poi.name,
+        description: poi.description,
+      }
+    });
+    setPois(pois);
+  }
+
+  useEffect(() => {
+    (async() => {
+      if (!loading)
+        getListPois();
+    })()
+  }, [loading])
 
   if (loading) {
     return <Loading />;
@@ -23,8 +48,14 @@ function App() {
 
   return (
     <div className="App">
-      <MainHeader logout={logout} isAuthenticated={isAuthenticated} loginWithRedirect={loginWithRedirect} />
-      <LeafletMapComponent/>
+      <MainHeader 
+        logout={logout} 
+        isAuthenticated={isAuthenticated} 
+        loginWithRedirect={loginWithRedirect} 
+      />
+      <LeafletMapComponent 
+        pois={pois}
+      />
       <SidePanelComponent />
       <FormDialog />
     </div>
