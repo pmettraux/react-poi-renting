867da20cec2ecb21a1bf2e8821db1f6217230938
Kysim <yosh97@hotmail.com>
2020-06-06 12:02:54 +0200

    correcting code

--- src/components/map-component/map.js
+++ src/components/map-component/map.js
@@ -49,7 +49,7 @@ export default function LeafletMapComponent() {
                 name: poi.name,
                 description: poi.description,
             }
-        },[]);
+        });
         setPois(pois);
     }
 
